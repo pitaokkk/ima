4941948376711db90dcef868740ea34894435a5b
pitaokkk <96687564+pitaokkk@users.noreply.github.com>
2024-06-10 17:11:44 +0800

    second commit

---
+++ app/(root)/credits/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CreditPage = () => {
+  return <div>Creditpage</div>;
+};
+
+export default CreditPage;
---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { UserButton } from '@clerk/nextjs';
+import { User } from '@clerk/nextjs/server';
+
+const Home = () => {
+  return (
+    <div>
+      <p>Home</p>
+    </div>
+  );
+};
+
+export default Home;
